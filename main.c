7faacc3b3675dcb530532ebd4f0aaa2c5be55159
Lucas Alves <sociallucasalves@gmail.com>
2024-05-11 18:51:33 -0300

    list planes feito

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ void main()
         printf("7 - Listar Próximas Decolagens\n");
         printf("8 - Listar Próximas Aterrissagens\n");
         printf("9 - Relatório Aeroporto\n");
+        printf("0 - Sair\n");
         printf("---------------------------------------\n");
         printf("Digite o número da operação:\n");
         scanf("%d", &choice);
@@ -31,14 +32,39 @@ void main()
 void excecute(Airport **airport, int choice)
 {
     char plane_id[5];
+    int i;
     switch (choice)
     {
     case 1:
         printf("Escreva o código do avião a ser inserido:\n");
         scanf("%s", &plane_id);
-        register_plane(&(*airport), plane_id);
+        if (register_plane(&(*airport), plane_id))
+        {
+            printf("Avião registrado com sucesso: %s\n", plane_id);
+        }
+        else
+        {
+            printf("Avião já estava registrado: %s\n", plane_id);
+        }
         break;
     case 2:
+        printf("Lista de aviões registrados:\n");
+        for (i = 0; i < 26; i++)
+        {
+            Airport *aaux = &(*airport);
+            if (aaux->ldisp[i].size > 0)
+            {
+                printf("%d:\n", i);
+                Node *aux = aaux->ldisp[i].head;
+                while (aux->next != NULL)
+                {
+                    printf("%s\n", aux->plane_id);
+                    aux = aux->next;
+                }
+                printf("%s\n", aux->plane_id);
+            }
+        }
+
         break;
     case 3:
         break;
